1c407cf2bccd1d80d4323751bbb3dfa46cf5f697
Tyler Haigh <tylerhaigh1994@me.com>
2014-11-19 21:10:43 +1100

    Adds DFS with test

--- src/main/java/algorithms/Search.java
+++ src/main/java/algorithms/Search.java
@@ -2,16 +2,10 @@ package algorithms;
 
 import java.util.ArrayList;
 
-import datastructures.BinaryTree;
+import datastructures.*;
 
 public class Search {
 
-	public static void DepthFirstSearch() {
-		int count1 = 0;
-		int count2 = 0;
-		
-	}
-	
 	public static <T extends Comparable<T>> int binarySearch(ArrayList<T> lst, T key) {
 		int start = 0;
 		int end = lst.size();
@@ -58,4 +52,30 @@ public class Search {
 		return -1;
 	}
 	
+	public static <T extends Comparable<T>> boolean depthFirstSearch(Graph g, int start, Vertex key) {
+		int n = g.size();
+		for (int i = 0; i < n; i++) {
+			Vertex v = g.getVertex(i);
+			v.setMarked(false);
+		}
+		
+		return depthFirstSearchRec(g, start, key);
+	}
+	
+	private static <T extends Comparable<T>> boolean depthFirstSearchRec(Graph g, int start, Vertex key) {
+		Vertex v = g.getVertex(start);
+		if (key.equals(v)) return true;
+		
+		boolean found = false;
+		v.setMarked(true);
+		
+		for (Vertex w : v.getAdjacentVertices()) {
+			if (!w.isMarked()) {
+				found = depthFirstSearchRec(g, g.getList().indexOf(w), key);
+				if (found) break;
+			}
+		}
+		
+		return found;
+	}
 }
--- src/test/java/test/SearchTests.java
+++ src/test/java/test/SearchTests.java
@@ -1,9 +1,13 @@
 package test;
 
 import org.junit.Test;
+
+import datastructures.Graph;
+import datastructures.Vertex;
 import static org.junit.Assert.*;
 
 import java.util.*;
+
 import algorithms.Search;
 
 public class SearchTests {
@@ -30,5 +34,34 @@ public class SearchTests {
 		lst.add(5);
 		
 		assertEquals(4, Search.binarySearchRec(lst, 5));
+	}
+	
+	@Test
+	public void testDepthFirstSearch() {
+		Graph g = new Graph();
+		
+		Vertex one = new Vertex();
+		Vertex two = new Vertex();
+		Vertex three = new Vertex();
+		Vertex four = new Vertex();
+		
+		/*
+		 * one -----> two
+		 *  |
+		 *  |
+		 *  |
+		 *  v
+		 *  three --> four
+		 */
+		
+		g.add(one, 0);
+		g.add(two, 0);
+		g.add(three, 0);
+		g.add(two, 1);
+		g.add(three, 2);
+		g.add(four, 2);
+		g.add(four, 3);
+		
+		assertEquals(true, Search.depthFirstSearch(g, 0, four));
 	}
 }
