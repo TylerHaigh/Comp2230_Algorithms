84699b48fad70353dcb3e93497646940fc778eb3
Tyler Haigh <tylerhaigh1994@me.com>
2014-11-22 15:29:44 +1100

    Adds Merge sort

--- src/main/java/algorithms/Sort.java
+++ src/main/java/algorithms/Sort.java
@@ -1,33 +1,84 @@
 package algorithms;
 
+import java.util.ArrayList;
 import java.util.List;
+
 import datastructures.Heap;
 
 public class Sort {
 
-	public static void MergeSort() {
-		
+	public static <T extends Comparable<T>> List<T> mergeSort(List<T> array) {
+		int start = 0;
+		int end = array.size() - 1;
+		return mergeSort(array, start, end);
 	}
 	
-	public static int ReturnOne() {
-		return 1;
+	private  static <T extends Comparable<T>> List<T> mergeSort(List<T> array, int start, int end) {
+		if(start == end) return array;
+		
+		int mid = (start + end) / 2;
+		array = mergeSort(array, start, mid);
+		array = mergeSort(array, mid + 1, end);
+		
+		return merge(array, start, mid, end);
+	}
+	
+	private static <T extends Comparable<T>> List<T> merge(List<T> array, int start, int mid, int end) {
+		int firstIndex = start;
+		int secondIndex = mid + 1;
+		
+		List<T> sortedList = new ArrayList<T>();
+		
+		while (firstIndex <= mid && secondIndex <= end) {
+			T first = array.get(firstIndex);
+			T second = array.get(secondIndex);
+			int compareVal = first.compareTo(second);
+			
+			if (compareVal == -1) {
+				sortedList.add(first);
+				firstIndex++;
+			} else {
+				sortedList.add(second);
+				secondIndex++;
+			}
+		}
+		
+		//Copy remainder of first half
+		while (firstIndex <= mid) {
+			sortedList.add(array.get(firstIndex));
+			firstIndex++;
+		}
+		
+		//Copy remainder of first half
+		while (secondIndex <= end) {
+			sortedList.add(array.get(secondIndex));
+			secondIndex++;
+		}
+		
+		int sortedIndex = 0;
+		for (int i = start; i <= end; i++) {
+			array.set(i, sortedList.get(sortedIndex));
+			sortedIndex++;
+		}
+		
+		return array;
+	}
+	
+	public static List heapSort(List array) {
+		Heap.heapify(array);
+
+		for (int i = array.size() - 1; i > 1; i--) {
+			swap(array, 0, i);
+
+			Heap.siftdown(array, 1);
+		}
+
+		return array;
 	}
 
-  public static List heapSort(List array) {
-    Heap.heapify(array);
-
-    for (int i = array.size() - 1; i > 1; i--) {
-      swap(array, 0, i);
-
-      Heap.siftdown(array, 1);
-    }
-
-    return array;
-  }
-
-  private static void swap(List array, int i, int j) {
-    Object temp = array.get(i);
-    array.set(i, array.get(j));
-    array.set(j, temp);
-  }
+	private static void swap(List array, int i, int j) {
+		Object temp = array.get(i);
+		array.set(i, array.get(j));
+		array.set(j, temp);
+	}
 }
--- src/test/java/test/SortTests.java
+++ src/test/java/test/SortTests.java
@@ -11,20 +11,28 @@ import java.util.*;
 public class SortTests {
 
 	@Test
-	public void testJUnit() {
-		int one = 1;
-		int val = Sort.ReturnOne();
-		
-		assertEquals(one,val);
+  	public void heapSortTest() {
+		Integer[] arr = new Integer[] { 5, 10, 1, 6, 3, 11, 2 };
+		List<Integer> list = Arrays.asList(arr);
+
+		List<Integer> sorted = Sort.heapSort(list);
+
+		assertEquals(1, (int)sorted.get(0));
 	}
+	
+	@Test
+	public void mergeSortTest() {
+		Integer[] arr = new Integer[] { 5, 10, 1, 6, 3, 11, 2 };
+		List<Integer> list = Arrays.asList(arr);
 
-  @Test
-  public void heapSortTest() {
-    Integer[] arr = new Integer[] { 5, 10, 1, 6, 3, 11, 2 };
-    List<Integer> list = Arrays.asList(arr);
+		List<Integer> sorted = Sort.mergeSort(list);
 
-    List<Integer> sorted = Sort.heapSort(list);
-
-    assertEquals(1, (int)sorted.get(0));
-  }
+		assertEquals(1, (int)sorted.get(0));
+		assertEquals(2, (int)sorted.get(1));
+		assertEquals(3, (int)sorted.get(2));
+		assertEquals(5, (int)sorted.get(3));
+		assertEquals(6, (int)sorted.get(4));
+		assertEquals(10, (int)sorted.get(5));
+		assertEquals(11, (int)sorted.get(6));
+	}
 }
