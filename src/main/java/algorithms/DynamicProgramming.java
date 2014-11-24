5cef28a408c4575b5b0c87245228a21fedef66b3
Tyler Haigh <tylerhaigh1994@me.com>
2014-11-24 14:38:28 +1100

    Adds DP Rook algorithm

--- src/main/java/algorithms/DynamicProgramming.java
+++ src/main/java/algorithms/DynamicProgramming.java
@@ -148,4 +148,27 @@ public class DynamicProgramming {
 		
 		return matrix;
 	}
+	
+	public static int[][] rook(int n, int m) {
+		
+		int[][] matrix = new int[n][m];
+		
+		//Setup
+		for (int i = 0; i < n; i++) {
+			matrix[i][0] = 1;
+		}
+		
+		for (int i = 0; i < m; i++) {
+			matrix[0][i] = 1;
+		}
+		
+		//Fill in matrix
+		for (int i = 1; i < n; i++) {
+			for (int j = 1; j < m; j++) {
+				matrix[i][j] = matrix[i-1][j] + matrix[i][j-1];
+			}
+		}
+		
+		return matrix;
+	}
 }
--- src/test/java/test/DynamicProgrammingTests.java
+++ src/test/java/test/DynamicProgrammingTests.java
@@ -127,4 +127,27 @@ public class DynamicProgrammingTests {
 			Assert.assertArrayEquals(expecteds[i], actuals[i]);
 		}
 	}
+	
+	@Test
+	public void testRook() {
+		int n = 3;
+		int m = n;
+		
+		int[][] expecteds = {
+				{1,1,1},
+				{1,2,3},
+				{1,3,6}
+			};
+		
+		int[][] actuals = DynamicProgramming.rook(n, m);
+		
+		for (int i = 0; i < actuals.length; i++) {
+			for (int j = 0; j < actuals[i].length; j++) {
+				System.out.print(actuals[i][j] + ",\t");
+			}
+			System.out.println();
+			
+			Assert.assertArrayEquals(expecteds[i], actuals[i]);
+		}
+	}
 }
